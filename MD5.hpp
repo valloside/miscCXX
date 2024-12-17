60aaf3108743effbf23a97d7041754746c2d81e4
valloside <61108284+valloside@users.noreply.github.com>
2024-12-17 21:49:32 +0800

    better impl

--- MD5.hpp
+++ MD5.hpp
@@ -2,33 +2,46 @@
 
 #include <cstdint>
 #include <array>
+#include <vector>
+#include <span>
+#include <string_view>
 
 namespace constexpr_hash
 {
-    // compile time MD5
     class MD5
     {
     public:
         template <size_t SIZE>
-        static constexpr std::array<uint32_t, 4> md5Hash(const char (&input)[SIZE] = "")
+        static constexpr std::array<uint32_t, 4> md5Hash(const char (&input)[SIZE])
         {
-            std::array paddedData = paddingData(input);
+            return md5Hash({input, SIZE - 1});
+        }
 
+        static constexpr std::array<uint32_t, 4> md5Hash(std::string_view input)
+        {
+            std::vector paddedData = paddingData(input);
             return processing(paddedData);
         }
 
 #ifdef __SIZEOF_INT128__
         template <size_t SIZE>
-        static constexpr __uint128_t md5HashAsUint128(const char (&input)[SIZE] = "")
+        static constexpr __uint128_t md5HashAsUint128(const char (&input)[SIZE])
+        {
+            return md5HashAsUint128({input, SIZE - 1});
+        }
+
+        static constexpr __uint128_t md5HashAsUint128(std::string_view input)
         {
             auto        md5Val = md5Hash(input);
             __uint128_t result = 0;
             for (int i = 0; i < 4; i++)
-                result = (result << 32)
-                         | ((md5Val[i] & 0x000000FF) << 24)
-                         | ((md5Val[i] & 0x0000FF00) << 8)
-                         | ((md5Val[i] & 0x00FF0000) >> 8)
-                         | ((md5Val[i] & 0xFF000000) >> 24);
+            {
+                result |= (__uint128_t)(((md5Val[i] & 0x000000FF) << 24)
+                                        | ((md5Val[i] & 0x0000FF00) << 8)
+                                        | ((md5Val[i] & 0x00FF0000) >> 8)
+                                        | ((md5Val[i] & 0xFF000000) >> 24))
+                       << ((3 - i) * 32);
+            }
             return result;
         }
 #endif
@@ -39,23 +52,24 @@ namespace constexpr_hash
             return (value << shift) | (value >> (32 - shift));
         }
 
-        template <size_t SIZE, size_t RET_BYTE_LENGTH = ((SIZE + 7) / 64 + 1) * 64>
-        static constexpr std::array<uint32_t, RET_BYTE_LENGTH / 4> paddingData(const char (&input)[SIZE])
+        static constexpr std::vector<uint32_t> paddingData(std::string_view input)
         {
-            std::array<uint8_t, RET_BYTE_LENGTH> buffer = {};
-            for (size_t i = 0; i < SIZE; ++i)
+            const size_t         size = input.size();
+            const size_t         RET_BYTE_LENGTH = ((size + 8) / 64 + 1) * 64;
+            std::vector<uint8_t> buffer(RET_BYTE_LENGTH, 0);
+            for (size_t i = 0; i < size + 1; ++i)
             {
-                buffer[i] = input[i];
+                buffer[i] = input.data()[i];
             }
-            buffer[SIZE - 1] = 0x80;
+            buffer[size] = 0x80;
 
-            constexpr size_t bit_len = (SIZE - 1) * 8;
+            size_t bit_len = size * 8;
             for (size_t i = 0; i < 8; ++i)
             {
                 buffer[RET_BYTE_LENGTH - 8 + i] = (bit_len >> (i * 8)) & 0xFF;
             }
 
-            std::array<uint32_t, RET_BYTE_LENGTH / 4> ret = {};
+            std::vector<uint32_t> ret(RET_BYTE_LENGTH / 4, 0);
             for (size_t i = 0; i < RET_BYTE_LENGTH; i += 4)
             {
                 ret[i / 4] = buffer[i] | (buffer[i + 1] << 8) | (buffer[i + 2] << 16) | (buffer[i + 3] << 24);
@@ -64,22 +78,25 @@ namespace constexpr_hash
             return ret;
         }
 
-        template <size_t SIZE>
-        static constexpr std::array<uint32_t, 4> processing(std::array<uint32_t, SIZE> &data)
+        static constexpr std::array<uint32_t, 4> processing(std::span<uint32_t> data)
         {
-            const std::array<uint32_t, 64> r = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-                                                5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
-                                                4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-                                                6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
-            const std::array<uint32_t, 64> k = {3614090360, 3905402710, 606105819, 3250441966, 4118548399, 1200080426, 2821735955, 4249261313,
-                                                1770035416, 2336552879, 4294925233, 2304563134, 1804603682, 4254626195, 2792965006, 1236535329,
-                                                4129170786, 3225465664, 643717713, 3921069994, 3593408605, 38016083, 3634488961, 3889429448,
-                                                568446438, 3275163606, 4107603335, 1163531501, 2850285829, 4243563512, 1735328473, 2368359562,
-                                                4294588738, 2272392833, 1839030562, 4259657740, 2763975236, 1272893353, 4139469664, 3200236656,
-                                                681279174, 3936430074, 3572445317, 76029189, 3654602809, 3873151461, 530742520, 3299628645,
-                                                4096336452, 1126891415, 2878612391, 4237533241, 1700485571, 2399980690, 4293915773, 2240044497,
-                                                1873313359, 4264355552, 2734768916, 1309151649, 4149444226, 3174756917, 718787259, 3951481745};
-            std::array<uint32_t, 4>        initVal = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476};
+            const size_t SIZE = data.size();
+
+            const std::array<uint32_t, 64> r =
+                {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+                 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+                 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+                 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+            const std::array<uint32_t, 64> k =
+                {3614090360, 3905402710, 606105819, 3250441966, 4118548399, 1200080426, 2821735955, 4249261313,
+                 1770035416, 2336552879, 4294925233, 2304563134, 1804603682, 4254626195, 2792965006, 1236535329,
+                 4129170786, 3225465664, 643717713, 3921069994, 3593408605, 38016083, 3634488961, 3889429448,
+                 568446438, 3275163606, 4107603335, 1163531501, 2850285829, 4243563512, 1735328473, 2368359562,
+                 4294588738, 2272392833, 1839030562, 4259657740, 2763975236, 1272893353, 4139469664, 3200236656,
+                 681279174, 3936430074, 3572445317, 76029189, 3654602809, 3873151461, 530742520, 3299628645,
+                 4096336452, 1126891415, 2878612391, 4237533241, 1700485571, 2399980690, 4293915773, 2240044497,
+                 1873313359, 4264355552, 2734768916, 1309151649, 4149444226, 3174756917, 718787259, 3951481745};
+            std::array<uint32_t, 4> initVal = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476};
             for (size_t block = 0; block < SIZE / 16; ++block)
             {
                 uint32_t a = initVal[0], b = initVal[1], c = initVal[2], d = initVal[3];
@@ -123,17 +140,15 @@ namespace constexpr_hash
 
     namespace hash_literals
     {
-        template <typename T, T... chars>
-        constexpr std::array<uint32_t, 4> operator""_MD5()
+        constexpr std::array<uint32_t, 4> operator""_MD5(const char *str, size_t size)
         {
-            return MD5::md5Hash({chars..., '\0'});
+            return MD5::md5Hash({str, size});
         }
 
 #ifdef __SIZEOF_INT128__
-        template <typename T, T... chars>
-        constexpr __uint128_t operator""_MD5_128()
+        constexpr __uint128_t operator""_MD5_128(const char *str, size_t size)
         {
-            return MD5::md5HashAsUint128({chars..., '\0'});
+            return MD5::md5HashAsUint128({str, size});
         }
 #endif
 
@@ -141,15 +156,19 @@ namespace constexpr_hash
 
 } // namespace constexpr_hash
 
-// void usage()
-// {
-//     constexpr std::array<uint32_t, 4> md5_result = md5Hash("hello");
+/*
 
-//     // if your compiler support __uint128_t
-//     constexpr __uint128_t             md5_result_128 = md5HashAsUint128("hello");
+void usage()
+{
+    constexpr std::array<uint32_t, 4> md5_result = md5Hash("hello");
 
-//     using namespace constexpr_hash::hash_literals;
-//     constexpr auto md5_result_128_2 = "hello"_MD5_128; // need GNU extension
+    // if your compiler support __uint128_t
+    constexpr __uint128_t md5_result_128 = md5HashAsUint128("hello");
 
-//     std::print("{:X}", md5_result_128); // 5D41402ABC4B2A76B9719D911017C592
-// }
+    using namespace constexpr_hash::hash_literals;
+    constexpr auto md5_result_128_2 = "hello"_MD5_128;
+
+    std::print("{:X}", md5_result_128); // 5D41402ABC4B2A76B9719D911017C592
+}
+
+*/
